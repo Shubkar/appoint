77796a0e99000fae18ad54bf2b2a5dc4336d7cf4
shubham k <shubkar.kadolkar@gmail.com>
2023-09-28 21:05:26 +0530

    RouteServiceProvider home route changed and logout route commented in API route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/reports/SummarySheet';
 
     /**
      * Define your route model bindings, pattern filters, etc.
--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::group([
 ], function ($router) {
 
 Route::post('login', 'AuthController@login');
-Route::post('logout', 'AuthController@logout');
+// Route::post('logout', 'AuthController@logout');
 Route::post('refresh', 'AuthController@refresh');
 Route::post('me', 'AuthController@me');
 Route::post('getAppointments','AuthController@getAppointments');
