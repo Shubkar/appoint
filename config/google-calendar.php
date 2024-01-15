d063be22ef77fe4b201856937dc322506a221b4e
shubham k <shubkar.kadolkar@gmail.com>
2024-01-15 11:11:41 +0530

    production update

--- config/google-calendar.php
+++ config/google-calendar.php
@@ -4,7 +4,7 @@ return [
     /*
      * Path to the json file containing the credentials.
      */
-    'service_account_credentials_json' => storage_path('app/google-calendar/service-acc-cred.json'),
+    'service_account_credentials_json' => storage_path('app/google-calendar/service-account-credentials.json'),
 
     /*
      *  The id of the Google Calendar that will be used by default.
